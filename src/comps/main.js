ac1ce8fa9faa15e76e3a03e21fafb2b2fa09c1da
Moonwalker <abhisk.mhra@gmail.com>
2021-04-30 00:17:01 +0530

    live sync of main and sidebar comp title and body

--- src/comps/main.js
+++ src/comps/main.js
@@ -1,13 +1,32 @@
-const MainBar = () => {
+const MainBar = ({activeNote,onUpdateNote}) => {
+    
+    const onEditField = (key,value) => {
+        onUpdateNote({
+            ...activeNote,
+            [key]:value,
+            lastModified: Date.now(),
+        });
+    }
+
+    if(!activeNote){
+        return <div className="no-active-note">No Notes Selected</div>
+    }
+
     return ( 
         <div className="app-main">
             <div className="app-main-note-edit">
-        <input type="text"  id="title" autoFocus/>
-           <textarea name="" id="body" placeholder="Write your notes here..."></textarea>
+        <input type="text"  id="title" 
+        value={activeNote.title}
+        onChange={(evt)=>onEditField("title",evt.target.value)}
+        autoFocus/>
+           <textarea name="" id="body"
+            value={activeNote.body}
+             onChange={(evt)=>onEditField("body",evt.target.value)}
+           placeholder="Write your notes here..."></textarea>
             </div>
             <div className="app-main-note-preview">
-             <h1 className="preview-title">Title</h1>
-            <div className="markdown-preview">Note Preview</div>
+             <h1 className="preview-title">{activeNote.title}</h1>
+            <div className="markdown-preview">{activeNote.body}</div>
             </div>
         </div>
      );
