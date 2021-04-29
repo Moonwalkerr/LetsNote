dc09e816bedf16ff996c94521097a6d2e8945149
Moonwalker <abhisk.mhra@gmail.com>
2021-04-29 23:38:25 +0530

    sidebar, mainbar comps imported and created notes state and passed props : deleteNote, onaddNote, notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import MainBar from './comps/main';
+import Sidebar from './comps/sidebar';
+import {useState} from "react";
+import uuid from "react-uuid";
+
 
 function App() {
+  const [notes, setnotes] = useState([]);
+
+  const onAddNote = () =>{
+    const newNote = {
+      id: uuid(),
+      title:"Untitled Note",
+      body: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      lastModified: Date.now(),  // JS Function 
+
+    }
+    setnotes([newNote,...notes]);
+  }
+
+  const deleteNote =(id)=>{
+    const newNotes = notes.filter(note=>note.id !== id);
+    setnotes(newNotes);
+  }
   return ( 
   <div className ="App">
-    <h1> Lets Note </h1> 
+    <Sidebar 
+    deleteNote={deleteNote}
+     onAddNote={onAddNote}
+     notes={notes}/>
+    <MainBar/>
     </div>
   );
 }
