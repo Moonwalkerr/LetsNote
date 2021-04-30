2821936a76185bf17ef9fa8bb63896ec7b4b4153
Moonwalker <abhisk.mhra@gmail.com>
2021-04-30 13:53:02 +0530

    firebaseConfig file added

---
+++ src/keys/firebaseConfig.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlRmZYcwZQPqJelKIFJG0xOPameLNv5So",
+  authDomain: "evernoteclone-88617.firebaseapp.com",
+  projectId: "evernoteclone-88617",
+  storageBucket: "evernoteclone-88617.appspot.com",
+  messagingSenderId: "795714751059",
+  appId: "1:795714751059:web:93cb11d9b1ba18d4ab14a6"
+};
+
+ firebase.initializeApp(firebaseConfig);
