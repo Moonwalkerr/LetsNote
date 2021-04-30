545129fddc1863f7e1c6558c49268c5c2a54a0df
Moonwalker <abhisk.mhra@gmail.com>
2021-04-30 14:50:39 +0530

    routes for auth components and private route added

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
-import './App.css';
-import MainBar from './comps/main';
-import Sidebar from './comps/sidebar';
-import {useState,useEffect} from "react";
-import uuid from "react-uuid";
-
+import React from 'react';
+import SignUpForm from './comps/signUpForm';
+import {AuthProvider} from './context/AuthContext';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "./comps/home";
+import LoginForm from './comps/login';
+import ForgotPaswd from "./comps/forgotPaswd";
+import PrvRoute from "./comps/privateRoute";
 
 function App() {
-  const [notes, setnotes] = useState(
-    localStorage.notes ? JSON.parse(localStorage.notes) : []
-  );
-  const [activeNote, setactiveNote] = useState(false);
-  const onAddNote = () =>{
-    const newNote = {
-      id: uuid(),
-      title:"Untitled Note",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
-      lastModified: Date.now(),  // JS Function 
-
-    }
-    setnotes([newNote,...notes]);
-  }
-
-  const deleteNote =(id)=>{
-    const newNotes = notes.filter(note=>note.id !== id);
-    setnotes(newNotes);
-  }
-
-  const getActiveNote = ()=>{
-    return notes.find((note)=>{
-      return note.id === activeNote
-      // will return obj of curr active note
-    })
-  }
-
-  const onUpdateNote = (updatedNote) =>{
-    const newNotesArray = notes.map((note)=>{
-      if(note.id === activeNote){
-        return updatedNote
-      }
-      return note;
-    })
-    setnotes(newNotesArray);
-  }
-
-
-  // Storing Notes in Local Storage of Browser
-
-  useEffect(()=>{
-    localStorage.setItem("notes",JSON.stringify(notes));
-  },[notes])
-
-  return ( 
-  <div className ="App">
-    <Sidebar 
-    activeNote={activeNote}
-    setactiveNote={setactiveNote}
-    deleteNote={deleteNote}
-     onAddNote={onAddNote}
-     notes={notes}/>
-    <MainBar
-    onUpdateNote={onUpdateNote}
-    activeNote={getActiveNote()} // this function needs to be running always to pass current active note
-    />
-    </div>
+  return (
+    <div className="App">
+     <Router>
+      <AuthProvider>
+        <Switch>
+          <PrvRoute exact path="/" component={Home}/>
+          <Route exact path="/login" component={LoginForm}/>
+          <Route exact path="/signup">
+          <SignUpForm/>
+          </Route>
+          <Route exact path="/forgotpassword"
+          component={ForgotPaswd}/>
+        </Switch>
+      </AuthProvider>
+     </Router>
+      </div>
   );
 }
 
---
+++ src/comps/home.js
@@ -0,0 +1,69 @@
+import '../App.css';
+import MainBar from './comps/main';
+import Sidebar from './comps/sidebar';
+import {useState,useEffect} from "react";
+import uuid from "react-uuid";
+
+
+function Home() {
+  const [notes, setnotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+  const [activeNote, setactiveNote] = useState(false);
+  const onAddNote = () =>{
+    const newNote = {
+      id: uuid(),
+      title:"Untitled Note",
+      body: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      lastModified: Date.now(),  // JS Function 
+
+    }
+    setnotes([newNote,...notes]);
+  }
+
+  const deleteNote =(id)=>{
+    const newNotes = notes.filter(note=>note.id !== id);
+    setnotes(newNotes);
+  }
+
+  const getActiveNote = ()=>{
+    return notes.find((note)=>{
+      return note.id === activeNote
+      // will return obj of curr active note
+    })
+  }
+
+  const onUpdateNote = (updatedNote) =>{
+    const newNotesArray = notes.map((note)=>{
+      if(note.id === activeNote){
+        return updatedNote
+      }
+      return note;
+    })
+    setnotes(newNotesArray);
+  }
+
+
+  // Storing Notes in Local Storage of Browser
+
+  useEffect(()=>{
+    localStorage.setItem("notes",JSON.stringify(notes));
+  },[notes])
+
+  return ( 
+  <div className ="Home">
+    <Sidebar 
+    activeNote={activeNote}
+    setactiveNote={setactiveNote}
+    deleteNote={deleteNote}
+     onAddNote={onAddNote}
+     notes={notes}/>
+    <MainBar
+    onUpdateNote={onUpdateNote}
+    activeNote={getActiveNote()} // this function needs to be running always to pass current active note
+    />
+    </div>
+  );
+}
+
+export default Home;
